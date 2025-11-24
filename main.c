92faeec900899d1595a9ef150be9f922811fb794
Armandas-K <armandas2005@gmail.com>
2025-11-24 23:12:45 +0000

    add main shell loop

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+void shell_loop();
+
+
+int main() {
+    shell_loop();
     return 0;
+}
+
+void shell_loop() {
+    // pointer to first char of zero terminated string
+    char *line;
+    // pointer to array of strings (char*)
+    char **args;
+    // output from executing command
+    int status = 1;
+
+    while (status) {
+        printf("shell> ");
+        fflush(stdout);
+
+        // line = read_line();             input
+        // args = parse_line(line);        tokenise
+        // status = execute_command(args); run command
+
+        free(line);
+        free(args);
+    }
 }
