9a63a78061ecc6f5a5060bd05f8c64a392aec595
Armandas-K <armandas2005@gmail.com>
2025-11-25 00:46:34 +0000

    add read line function

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+// maximum character input per command line
+// 8192 bits = 1024 chars
+#define MAX_INPUT 8192
 
 void shell_loop();
-
+char *read_line();
 
 int main() {
     shell_loop();
@@ -22,11 +27,61 @@ void shell_loop() {
         printf("shell> ");
         fflush(stdout);
 
-        // line = read_line();             input
+        line = read_line();
         // args = parse_line(line);        tokenise
         // status = execute_command(args); run command
 
         free(line);
         free(args);
+    }
+}
+
+char *read_line() {
+    // allocate buffer to store input
+    char *buffer = malloc(MAX_INPUT);
+
+    // check if malloc succeeds
+    if (!buffer) {
+        // print error
+        fprintf(stderr, "Error: malloc failed in read_line(): ");
+        perror(NULL);
+        exit(EXIT_FAILURE);
+    }
+
+    int pos = 0;
+
+    // loop through each char
+    while (1) {
+        char c;
+        // read syscall on stdin (file num 0)
+        ssize_t bytes_read = read(0, &c, 1);
+
+        // read returns 0 means EOF (ctrl+d)
+        if (bytes_read == 0) {
+            printf("\n");
+            exit(0);
+        }
+
+        // read returns error
+        if (bytes_read < 0) {
+            fprintf(stderr, "Error: read failed in read_line(): ");
+            perror(NULL);
+            exit(EXIT_FAILURE);
+        }
+
+        // if newline is read, terminate string and return buffer
+        if (c == '\n') {
+            buffer[pos] = '\0';
+            return buffer;
+        }
+
+        // store character, increment pos
+        buffer[pos++] = c;
+
+        // prevent buffer overflow
+        if (pos >= MAX_INPUT - 1) {
+            buffer[pos] = '\0';
+            return buffer;
+        }
     }
 }
